49599e1e10774eabfbce63ee642c949a7f798584
Ioannis Daniil <jondan97@gmail.com>
2022-07-13 18:40:55 +0300

    added logger to collection

--- java-path-core-collection/src/main/java/gr/codelearn/core/showcase/collection/CollectionApplication.java
+++ java-path-core-collection/src/main/java/gr/codelearn/core/showcase/collection/CollectionApplication.java
@@ -1,5 +1,8 @@
 package gr.codelearn.core.showcase.collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -11,6 +14,8 @@ import java.util.List;
 import java.util.Set;
 
 public class CollectionApplication {
+	private static final Logger logger = LoggerFactory.getLogger(CollectionApplication.class);
+
 	public static void main(String[] args) {
 		//arraysShowcase();
 		//arrayListShowcase();
@@ -24,11 +29,11 @@ public class CollectionApplication {
 		final HashMap<String, Long> objectObjectHashMap = new HashMap<>();
 		objectObjectHashMap.put("Collections", 0L);
 
-		System.out.println(objectObjectHashMap.get("Collections"));
+		logger.info("{}", objectObjectHashMap.get("Collections"));
 
 		objectObjectHashMap.put("Collections", objectObjectHashMap.get("Collections") + 1);
 
-		System.out.println(objectObjectHashMap.get("Collections"));
+		logger.info("{}", objectObjectHashMap.get("Collections"));
 
 		objectObjectHashMap.getOrDefault("Collections", 0L);
 	}
@@ -38,9 +43,9 @@ public class CollectionApplication {
 		objects.offer("A");
 		objects.offer("B");
 		objects.offer("C");
-		System.out.println(objects);
-		System.out.println(objects.poll());
-		System.out.println(objects);
+		logger.info("{}", objects);
+		logger.info("{}", objects.poll());
+		logger.info("{}", objects);
 	}
 
 	private static void setShowcase(){
@@ -55,7 +60,7 @@ public class CollectionApplication {
 		names.add("Jan");
 		names.add("Jan");
 
-		System.out.println(names);
+		logger.info("{}", names);
 	}
 
 	private static void arrayListShowcase() {
@@ -73,20 +78,20 @@ public class CollectionApplication {
 		integers.add(8);
 		integers.set(3, 2);
 		integers.add(0, 100);
-		System.out.println(integers);
+		logger.info("{}", integers);
 
 		integers.clear();
 		List<Integer> newIntegers = List.of(2, 3);
 		integers.add(1);
 		integers.add(2);
 		integers.addAll(newIntegers);
-		System.out.println(integers);
+		logger.info("{}", integers);
 		integers.removeAll(newIntegers);
-		System.out.println(integers);
+		logger.info("{}", integers);
 
 
 		final ArrayList<Object> objects1 = new ArrayList<>(100);
-		System.out.println(objects1.size());
+		logger.info("{}", objects1.size());
 
 		final ArrayList<String> names = new ArrayList<>();
 		names.add("Ioannis");
@@ -95,11 +100,11 @@ public class CollectionApplication {
 		names.add("Jan");
 		names.add("Ioanna");
 
-		System.out.println(names);
+		logger.info("{}", names);
 
 		Collections.sort(names);
 
-		System.out.println(names);
+		logger.info("{}", names);
 
 		names.sort(new Comparator<String>() {
 			@Override
@@ -111,7 +116,7 @@ public class CollectionApplication {
 			}
 		});
 
-		System.out.println(names);
+		logger.info("{}", names);
 
 
 	}
@@ -131,24 +136,24 @@ public class CollectionApplication {
 		integers.add(8);
 		integers.set(3, 2);
 		integers.add(0, 100);
-		System.out.println(integers);
+		logger.info("{}", integers);
 	}
 
 	private static void arraysShowcase(){
 		int[] ints = new int[10];
-		System.out.println(ints[0]);
+		logger.info("{}", ints[0]);
 
 		final String[] strings = new String[10];
-		System.out.println(strings[0]);
+		logger.info("{}", strings[0]);
 
 		int[] ints1 = {10, 0, 50, 25, 73};
 
 		for (int i = 0; i < ints1.length; i++) {
-			System.out.println(ints1[i]);
+			logger.info("{}", ints1[i]);
 		}
 
 		for (int i : ints1) {
-			System.out.println(i);
+			logger.info("{}", i);
 		}
 	}
 
